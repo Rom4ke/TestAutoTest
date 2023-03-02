c2ea22bb62b737f3f6a1ea38cca920c09a92463a
Roman <roman17.work@gmail.com>
2023-03-02 11:48:18 +0300

    add script for starts tests

--- jenkins.groovy
+++ jenkins.groovy
@@ -28,6 +28,34 @@ node {
     }
 }
 
+def getTestStages(testTags) {
+    def stages = [:]
+    testTags.each { tag ->
+        stages ["${tag}"] = {
+            returnTestWithTag(tag)
+        }
+    }
+    return stages
+}
+
+def runTestWithTag (String tag) {
+    try {
+        labelledShell(label: "Run ${tag}", script: "chmod +x gradlew \n./gradlew -x test ${tag}")
+    } finally {
+        echo "some failed tests"
+    }
+}
+
+def getProject (String repo, String branch) {
+    cleanWs()
+    checkout scm: [
+            $class : 'GitSCM', branches: [[name: branch]],
+            userRemoteConfigs: [[
+                    url: repo
+            ]]
+    ]
+}
+
 def generateAllure() {
     allure([
             includeProperties: true,
