8772bf379064ffe9b1e339b0fdbcc740b812feb2
Roman <roman17.work@gmail.com>
2023-03-02 11:25:34 +0300

    add groovy file

---
+++ jenkins.groovy
@@ -0,0 +1,22 @@
+task_branch = "${TEST_BRANCH_NAME}"
+
+def branch_cutted = task_branch.contains("origin") ? task_branch.split('/')[1] : task_branch.trim()
+currentBuild.displayName = "$branch_cutted"
+base_git_url = "https://github.com/Rom4ke/TestAutoTest.git"
+
+node {
+    withEnv(["branch=${branch_cutted}", "base_url=${base_git_url}"]) {
+        stage("Checkout Branch") {
+            if (!"$branch_cutted".contains("main")) {
+                try {
+                    getProject("$base_git_url", "$branch_cutted")
+                } catch (err) {
+                    echo "Failed get branch $branch_cutted"
+                    throw ("${err}")
+                }
+            } else {
+                echo "Current branch is main"
+            }
+        }
+    }
+}
